9081e2bf44afe6485d1bdb954d83633d7c524921
anubhav maurya <channelsub47@gmail.com>
2026-03-27 20:55:25 +0530

    services updated

--- InventorySearch/src/services/api.js
+++ InventorySearch/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://invantorysearchapi-backend.onrender.com/",
 });
 
 export const fetchSearch = (params) => API.get("/search", { params });
